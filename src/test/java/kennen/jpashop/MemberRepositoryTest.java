19fdafd8397e00a1c14a8d4c6d32a067b20f002e
kennen <kkt0929@midasit.com>
2019-11-15 20:45:28 +0900

    Junit5

--- src/test/java/kennen/jpashop/MemberRepositoryTest.java
+++ src/test/java/kennen/jpashop/MemberRepositoryTest.java
@@ -1,15 +1,15 @@
 package kennen.jpashop;
 
 import org.assertj.core.api.Assertions;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.mockito.Incubating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 
-@RunWith(SpringRunner.class)
+@Incubating
 @SpringBootTest
 public class MemberRepositoryTest {
 
@@ -19,6 +19,7 @@ public class MemberRepositoryTest {
 
     @Test
     @Transactional
+    @Rollback(value = false)
     public void testMember() {
         //given
         Member member = new Member();
@@ -30,6 +31,7 @@ public class MemberRepositoryTest {
 
         //then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember).isEqualTo(member);
 
 
     }
