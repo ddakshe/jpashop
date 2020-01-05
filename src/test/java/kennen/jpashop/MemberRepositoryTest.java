10a6f064afba8c87d67fa618f02a207258faa342
ddakshe <ddakshe@gmail.com>
2020-01-05 22:43:33 +0900

    - add Test Case - refactoring package

--- src/test/java/kennen/jpashop/MemberRepositoryTest.java
+++ src/test/java/kennen/jpashop/MemberRepositoryTest.java
@@ -1,38 +1,63 @@
 package kennen.jpashop;
 
-import kennen.jpashop.db.MemberRepository;
-import kennen.jpashop.db.domain.item.Member;
-import org.assertj.core.api.Assertions;
+import kennen.jpashop.domain.item.Member;
+import kennen.jpashop.repository.MemberRepository;
+import kennen.jpashop.service.MemberService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
-@SpringBootTest
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+
 @RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
 public class MemberRepositoryTest {
 
+
     @Autowired
-    MemberRepository memberRepository;
+    private MemberService memberService;
+    @Autowired
+    private MemberRepository memberRepository;
+    @PersistenceContext
+    private EntityManager em;
 
     @Test
-    @Transactional
-    @Rollback(false)
-    public void testMember() {
+    public void join() {
         //given
         Member member = new Member();
-        member.setName("memberA");
+        member.setName("kim");
 
         //when
-        Long savedId = memberRepository.save(member);
-        Member findMember = memberRepository.find(savedId);
+        Long savedId = memberService.join(member);
 
-        Assertions.assertThat(findMember.getId()).isEqualTo(savedId);
-        Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
-        Assertions.assertThat(findMember).isEqualTo(member);
+        //then
+        assertEquals(member, memberRepository.findOne(savedId));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void duplicate_join() {
+        //given
+        Member member1 = new Member();
+        member1.setName("kim1");
+
+        Member member2 = new Member();
+        member2.setName("kim1");
+
+        //when
+        memberService.join(member1);
+        memberService.join(member2);
+
+
+        //then
+        fail("예외가 발생해야 한다.");
     }
 }
